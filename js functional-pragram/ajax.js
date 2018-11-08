f295175d162c2095fa78b15d3dcbc92839049bfe
john <1020288416@qq.com>
2018-11-08 23:58:24 +0800

    map

--- js functional-pragram/ajax.js
+++ js functional-pragram/ajax.js
@@ -57,3 +57,22 @@ function ajaxFunction() {
         }
     }
 }
+
+
+var Container = function(x){
+    this.__value =x
+}
+
+//函数式编程一般约定,函子有一个of方法
+
+Container.of = x => new Container(x);
+//Container.of('abcd');
+//一般约定,函子的标志就是容器具有map方法.该方法将容器里面的每一个值,映射到另一个容器
+
+Container.prototype.map = function(f){
+    return Container.of(f(this.__value))
+}
+
+Container.of(3)
+    .map(x=>x+1)  //=>Container(4)
+    .map(x=>'Result is'+x)  //=>Container('Result is 4')
