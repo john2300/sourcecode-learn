6844cacff702e1ad94566df97c921b0d310465c7
john2300 <1020288416@qq.com>
2018-12-14 17:52:05 +0800

    work03

--- 临时/work02.js
+++ 临时/work02.js
@@ -1,14 +1,66 @@
-let a='3445944905'
 
-// String.prototype.forEach = Array.prototype.forEach
-String.prototype.forEach.bind(Array.prototype.forEach)
-Array.prototype.forEach.call(a,function(){
-    console.log()
-})
+// String.prototype.forEach = function(cb){
+//     for(let i=0;i<this.length;i++){
+//         cb&&cb(this[i])
+//     }
+// }
+// String.prototype.map = Array.prototype.map
+// String.prototype.map = function(cb){
+//     let arr=[]
+//     for(let i=0;i<this.length;i++){
+//         let r = cb&&cb(this[i])
+//         arr.push(r||this[i])
+//     }
+//     return arr
+// }
 
-a.forEach(item=>{
-    console.log(item)
-})
+// let str = '1233444'.map(r=>{
+//     if(r==2){
+//         return '*'
+//     }
+// })
 
+// console.log(str)
+// function formate(a){
+//      Array.prototype.forEach.call(a,function(item,index){
+//         console.log(item)
+//     })
+// }
 
+// formate('1233334')
 
+// function formate(a){
+//     let arr=[]
+//     for(let i of a){
+//         arr.push(i)
+//     }
+//     arr.forEach(r=>{
+//         console.log(r)
+//     })
+// }
+// formate('203034045i')
+
+// function formate(a){
+//     a.split('').forEach(r=>{
+//         console.log(r)
+//     })
+// }
+// formate('1343543254')
+
+let str = "fdskldksdmfWUWWUWUP()S]]][[NMXsdmfl     dsfmlkdskl"
+let obj = {}
+for(let i of str){
+    let code = i.charCodeAt()
+    if(code>64&&code<123){
+        if(obj[code]){
+            obj[code] +=1
+        }else{
+            obj[code] = 1
+        }
+    }
+}
+
+for(let k in obj){
+    let b = String.fromCharCode(k)
+    console.log(b+':'+obj[k])
+}
--- 临时/work03.js
+++ 临时/work03.js
@@ -1,18 +1,30 @@
-let arr=[
-    1,2,3,788,324,323,[
-        0230202,404434,21,0404,[
-            [
-                23,79,60,34,97,006,3748,[
-                    93939,1,8,928,0302,2002,111,[
-                        1828,[18839],[100202,030,[
-                            234,999,[233]
-                        ],202,1883,]
-                    ]
-                ],237
-            ],2033,12,45,23
-        ],2304
-    ],2
-]
+let arr1=[1,2,3,788,324,323,[0230202,404434,21,0404,[[23,79,60,34,97,006,3748,[93939,1,8,928,0302,2002,111,[1828,[18839],[100202,030,[234,999,[233]],202,1883,]]],237],2033,12,45,23],2304],2]
 
-console.log(JSON.stringify(arr))
-console.log(arr)
+function search(item,arr){
+    let low = 0
+        high = arr.length-1,
+        mid=0,element=0;
+    while(low<=high){
+        // console.log(11111111111111111)
+        mid =Math.floor((low+high)/2);
+        // console.log(mid)
+        if(arr[mid] instanceof Array){
+            arr[]
+        }
+        element = arr[mid];
+        if(element<item){
+            low = mid + 1;
+        }else if(element>item){
+            high = mid-1;
+        }else{
+            return mid;
+        }
+    }
+    return -1;
+}
+console.log(arr1 instanceof Array);
+console.log(search(0230202,arr1));
+
+// arr1.forEach(r=>{
+//     console.log(r);
+// })
