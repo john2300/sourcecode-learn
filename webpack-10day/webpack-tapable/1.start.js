3f61bec963b1e23497729e40ae0663c33f785400
john2300 <1020288416@qq.com>
2019-06-01 22:39:46 +0800

    20190601

--- webpack-10day/webpack-tapable/1.start.js
+++ webpack-10day/webpack-tapable/1.start.js
@@ -1,54 +1,60 @@
-//SyncHook为同步钩子
-// let {SyncHook} = require('tapable')
+// SyncHook为同步钩子
+let {SyncHook} = require('tapable')
+
+class Lesson {
+  constructor () {
+    // 自定义的钩子函数
+    this.hooks = {
+      arch: new SyncHook(['name']), // 实例的钩子方法
+    }
+  }
+  tap () { // 注册监听函数,钩子函数启动时,依次执行this的上的钩子方法
+    this.hooks.arch.tap('node', function (name) {
+      console.log('node', name)
+    })
+    this.hooks.arch.tap('react', function (name) {
+      console.log('react', name)
+    })
+  }
+  start () { // 启动钩子的方法
+    this.hooks.arch.call('jw')
+  }
+}
+
+let l = new Lesson()
+l.tap(); // 注册这些事件
+l.start(); // 启动钩子
+
+// -----------------------
+// SyncBallHook同步加一个保险,术语叫熔断,决定是否向下执行
+// let {SyncBailHook} = require('tapable')
 
 // class Lesson {
 //   constructor(){
 //     this.hooks = {
-//       arch:new SyncHook(['name']),//实例的钩子方法
+//       arch:new SyncBailHook(['name']),
 //     }
 //   }
 //   tap(){//注册监听函数,钩子函数启动时,依次执行this的上的钩子方法
 //     this.hooks.arch.tap('node',function(name){
-//       console.log('node',name);
-//     });
+//       console.log('node',name)
+//       // return undefined
+//       return '想停止学习';//遇到返回的值是非undefined时,返回结束向下执行
+//     })
 //     this.hooks.arch.tap('react',function(name){
-//       console.log('react',name);
-//     });
+//       console.log('react',name)
+//     })
 //   }
 //   start(){//启动钩子的方法
-//     this.hooks.arch.call('jw');
+//     this.hooks.arch.call('jw')
 //   }
 // }
 
-// let l = new Lesson();
+// let l = new Lesson()
 // l.tap();//注册这些事件
 // l.start();//启动钩子
 
-//-----------------------
-//SyncBallHook同步加一个保险,术语叫熔断,决定是否向下执行
-let {SyncBailHook} = require('tapable')
-
-class Lesson {
-  constructor(){
-    this.hooks = {
-      arch:new SyncBailHook(['name']),
-    }
-  }
-  tap(){//注册监听函数,钩子函数启动时,依次执行this的上的钩子方法
-    this.hooks.arch.tap('node',function(name){
-      console.log('node',name);
-      // return undefined;
-      return '想停止学习';//遇到返回的值是非undefined时,返回结束向下执行
-    });
-    this.hooks.arch.tap('react',function(name){
-      console.log('react',name);
-    });
-  }
-  start(){//启动钩子的方法
-    this.hooks.arch.call('jw');
-  }
-}
-
-let l = new Lesson();
-l.tap();//注册这些事件
-l.start();//启动钩子
+/**
+ * 
+ * 写一个功能,需要很多钩子函数,
+ */
